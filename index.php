23daa08fb269f7d5dca219bc5fa754d33e508422
Triesha Sonarkar <147965443+trieshasonarkar1224@users.noreply.github.com>
2025-01-24 19:34:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$name = "Code With Trish";
+$name = "Code With Dary"; 
+$sentence = "What's up YouTube"; // String 
+$age = 27; // Integer 
+$ageMinus= 27; // Integer 
+$price =18.58; // Float 
+$isTrue = true; // Boolean 
+$isFalse = false; 
+$brands = array('Apple', 'Microsoft', 'Huawei', 10); 
+$clubs = [
+    ' BAR' => 'Barcelona' 
+];
+
+$isEmpty = NULL; // NULL 
+
+var_dump($clubs);
